92ab0c82b4a152446e156063614e596dca9879c5
Eric Loving <ericdloving@gmail.com>
2022-07-31 09:53:54 -0500

    running man moved outside of text

--- src/components/header.js
+++ src/components/header.js
@@ -8,8 +8,8 @@ const Header = ({ user, setUser }) => {
   return (
     <header>
       <div id="logowrapper">
-        <h1 className="logotext f">F</h1>
-        <h1 className="logotext tail">tness Trackr</h1>
+        <h1 className="logotext head">Fitness</h1>
+        <h1 className="logotext tail">Trackr</h1>
         <img src="../../Images/stick-man-running.png" id="logoman"></img>
         <TabBar />
       </div>
